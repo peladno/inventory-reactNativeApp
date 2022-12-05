0ef4a433576218ea11571a008e0b1093a62e1084
Javier <j.perezurrutia@gmail.com>
2022-12-05 00:54:54 -0300

    fix

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { View } from 'react-native';
+
+import { Categories } from './screens';
+import { styles } from './styles';
+
+export default function App() {
+  return <Categories />;
+}
---
+++ src/screens/categories/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function Categories() {
+  return (
+    <View>
+      <Text>Categories</Text>
+    </View>
+  );
+}
+
+export default Categories;
