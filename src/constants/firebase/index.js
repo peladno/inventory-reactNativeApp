02bf6fa08301338a6647c8a810510116dd2af617
Javier <j.perezurrutia@gmail.com>
2023-01-09 16:58:43 -0300

    [feature] - adding env variables

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,4 +1,4 @@
-const API_KEY = '';
+import { API_KEY } from '@env';
 
 export const URL_AUTH_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
 export const URL_AUTH_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
