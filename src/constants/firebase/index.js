b3a976f1a5f91473562002493fa4ceee6b03f42c
Javier <j.perezurrutia@gmail.com>
2023-01-15 00:57:51 -0300

    [fix] - fix redux

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,6 +1,4 @@
-//import { API_KEY } from '@env';
-
-const API_KEY = 'AIzaSyAz_oGqWWzT9KYY2tBhxVbhoAHr-AX098A';
+import { API_KEY } from '@env';
 
 export const URL_AUTH_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
 export const URL_AUTH_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
--- src/store/auth.slicer.js
+++ src/store/auth.slicer.js
@@ -1,69 +1,15 @@
-<<<<<<< HEAD
-import { createSlice } from '@reduxjs/toolkit';
-
-import { signIn, signUp } from './actions';
-=======
-<<<<<<<< HEAD:src/store/actions/auth.action.js
-import { createAsyncThunk } from '@reduxjs/toolkit';
-
-import { URL_AUTH_SIGN_UP, URL_AUTH_SIGN_IN } from '../../constants/firebase';
-
-export const signUp = createAsyncThunk(
-  'auth/signUp',
-========
 import { createSlice } from '@reduxjs/toolkit';
 
 import { URL_AUTH_SIGN_IN, URL_AUTH_SIGN_UP } from '../constants/firebase';
->>>>>>> reduxtoolkit-test
 
 const initialState = {
   token: null,
   userId: null,
-<<<<<<< HEAD
-  error: null,
-  success: false,
-  loading: false,
-=======
->>>>>>> reduxtoolkit-test
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-<<<<<<< HEAD
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(signUp.pending, (state) => {
-      state.loading = true;
-      state.error = null;
-    });
-    builder.addCase(signUp.fulfilled, (state, action) => {
-      state.loading = false;
-      state.error = null;
-      state.userId = action.payload.userId;
-    });
-    builder.addCase(signUp.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.payload;
-    });
-    builder.addCase(signIn.pending, (state) => {
-      state.loading = true;
-      state.error = null;
-    });
-    builder.addCase(signIn.fulfilled, (state, action) => {
-      state.loading = false;
-      state.error = null;
-      state.userId = action.payload.userId;
-    });
-    builder.addCase(signIn.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.error;
-    });
-  },
-});
-
-export default authSlice.reducer;
-=======
   reducers: {
     login: (state, action) => {
       state.userId = action.payload;
@@ -77,62 +23,8 @@ export default authSlice.reducer;
 });
 
 export const { login, register } = authSlice.actions;
->>>>>>>> reduxtoolkit-test:src/store/auth.slicer.js
 
-  async (email, password) => {
-    try {
-      const response = await fetch(URL_AUTH_SIGN_UP, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email,
-          password,
-          returnSecureToken: true,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
-
-      const data = await response.json();
-      return { token: data.idToken, userId: data.localId };
-    } catch (error) {
-      throw error;
-    }
-  }
-);
-
-export const signIn = createAsyncThunk(
-  'auth/signIn',
-
-  async (email, password) => {
-    try {
-      const response = await fetch(URL_AUTH_SIGN_IN, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email,
-          password,
-          returnSecureToken: true,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
-
-      const data = await response.json();
-      return { token: data.idToken, userId: data.localId };
-    } catch (error) {
-      throw error;
-    }
-  }
-);
-
-/*export const signUp = (email, password) => {
+export const signUp = (email, password) => {
   return async (dispatch) => {
     try {
       const response = await fetch(URL_AUTH_SIGN_UP, {
@@ -190,11 +82,6 @@ export const signIn = (email, password) => {
       throw error;
     }
   };
-<<<<<<<< HEAD:src/store/actions/auth.action.js
-};*/
-========
 };
 
 export default authSlice.reducer;
->>>>>>>> reduxtoolkit-test:src/store/auth.slicer.js
->>>>>>> reduxtoolkit-test
